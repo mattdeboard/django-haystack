eb469ef8146a661d1caa987e9089d48f03f11fd7
Matt DeBoard <matt.deboard@gmail.com>
2012-06-15 15:18:05 -0400

    Add support for Django ORM-like 'extras' method to specify extra search parameters that will not be processed in any way, except to be passed directly to Solr at search-time.

--- haystack/backends/solr_backend.py
+++ haystack/backends/solr_backend.py
@@ -259,6 +259,7 @@ class SolrSearchBackend(BaseSearchBackend):
             # kwargs['fl'] += ' _dist_:geodist()'
             pass
 
+        kwargs.update(extras)        
         return kwargs
 
     def more_like_this(self, model_instance, additional_query_string=None,
@@ -492,6 +493,23 @@ class SolrSearchBackend(BaseSearchBackend):
 
 
 class SolrSearchQuery(BaseSearchQuery):
+    def __init__(self, **kwargs):
+        super(SolrSearchQuery, self).__init__(**kwargs)
+        self.extras = {}
+
+    def _clone(self, **kwargs):
+        clone = super(SolrSearchQuery, self)._clone(**kwargs)
+        clone.extras = self.extras.copy()
+        return clone
+
+    def add_extras(self, extras):
+        """
+        Adds any 'extra' search params & values to a query at
+        search-time.
+
+        """
+        self.extras.update(extras)
+        
     def matching_all_fragment(self):
         return '*:*'
 
@@ -668,6 +686,8 @@ class SolrSearchQuery(BaseSearchQuery):
 
         if self.distance_point:
             search_kwargs['distance_point'] = self.distance_point
+
+        search_kwargs['extras'] = self.extras
 
         results = self.backend.search(final_query, **search_kwargs)
         self._results = results.get('results', [])
